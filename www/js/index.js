547757d0a7328800addf8bca05cb8d18c117727e
sebastiaoRealino <sebastiaorealino@yahoo.com.br>
2017-10-26 00:53:38 -0200

    feat(push_notification): add push notification for project

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,33 @@
-/*var app = {
-    
-        initialize: function() {
-            document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        },
-    
-        // deviceready Event Handler
-        //
-        // Bind any cordova events here. Common events are:
-        // 'pause', 'resume', etc.
-        onDeviceReady: function() {
-            //myApp.alert('Exemplo modal', 'Exemplo modal!');
-        }
-    }; 
-    
-app.initialize();*/
-    
+var app = {
+
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function () {
+        //myApp.alert('Exemplo modal', 'Exemplo modal!');
+        this.receivedEvent('deviceready');
+    },
+    receivedEvent: function (id) {
+        window.FirebasePlugin.getInstanceId( 
+            (token) => {
+                console.log('token: ' + token);
+            }, (error) => {
+                console.log('token: ' + error);
+                alert(error);
+            });
+        window.FirebasePlugin.subscribe("example");
+
+        window.FirebasePlugin.onNotificationOpen((notification) => {
+            alert(JSON.stringify(notification));
+        }, (error) => {
+            alert(error);
+        });
+    }
+};
+
+app.initialize();
